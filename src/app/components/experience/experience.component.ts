ce98cfcb54fbd367c9c1977adb85449051bfe58c
Timothy jan <jan.timothy@gmail.com>
2023-08-30 14:08:10 -0700

    Updated Experience and Resume

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -25,7 +25,7 @@ export class ExperienceComponent implements OnInit{
       "September 2013 - June 2017",
       "B.S IN ELECTRICAL ENGINEERING",
       [
-        "Following my father and his father footsteps..."
+        "Following my father's and his father's footsteps..."
       ]
     );
     let job1 = new Job(
@@ -34,9 +34,9 @@ export class ExperienceComponent implements OnInit{
       "July 2017 - June 2018",
       "Software Test Engineer",
       [
-        "Conducted test protocols and simulations for the allergy/autoimmune antibody testing software and instruments.",
-        "Utilized TESTTRACK software for bug/defect tracking and documentation.",
-        "Configured PCs with necessary software and hardware to operate allergy/autoimmune antibody testing software/instruments."
+        "Executed meticulous test protocols and simulations for the allergy/autoimmune antibody testing software and associated instruments, ensuring the reliability and accuracy of critical medical diagnostics.",
+        "Proficiently employed TESTTRACK software to efficiently track and document bugs and defects, facilitating the streamlined management of software quality.",
+        "Expertly configured personal computers, integrating essential software and hardware components, to facilitate the smooth operation of allergy/autoimmune antibody testing software and instruments."
       ]
     );
     let job2 = new Job(
@@ -45,9 +45,9 @@ export class ExperienceComponent implements OnInit{
       "June 2018 - January 2019",
       "Electrical Engineer I",
       [
-        "Executed and modified sequences/modules using in-house development tools for developing and testing implantable medical devices in St. Jude Medical.",
-        "Troubleshot medical devices to identify and isolate firmware/software issues using data acquisition/measurement devices.",
-        "Analyzed data to generate Quality and R&D reports for future product improvement."
+        "Diagnosed medical devices to discern and isolate firmware and software issues using data acquisition and measurement equipment, including oscilloscopes, digital multimeters (DMMs), and arbitrary waveform generators (AWGs).",
+        "Employed data analysis techniques to create comprehensive Quality and R&D reports, contributing to the refinement and advancement of future product iterations.",
+        "Developed and refined sequences and modules utilizing proprietary development tools, integral to the advancement and testing of implantable medical devices within the St. Jude Medical division."
       ]
     );
     let job3 = new Job(
@@ -56,8 +56,9 @@ export class ExperienceComponent implements OnInit{
       "June 2018 - January 2019",
       "Failure Analysis Engineer II",
       [
-        "Implemented different in-depth examinations/troubleshooting for returned medical devices with issues to identify the root cause.",
-        "Evaluated device performances, images, interrogations, and longevity using in-house tools to produce analysis reports."
+        "Conducted thorough and tailored examinations and troubleshooting on a variety of returned medical devices, utilizing specialized techniques to pinpoint underlying issues and customer complaints.",
+        "Employed a range of in-house tools to assess device performance, images, interrogations, and longevity, contributing to the production of comprehensive analysis reports.",
+        "Played a pivotal role in identifying root causes of device-related concerns, driving the enhancement of product reliability and overall quality."
       ]
     );
     let job4 = new Job(
@@ -66,9 +67,9 @@ export class ExperienceComponent implements OnInit{
       "March 2019 - November 2020",
       "Process Engineer",
       [
-        "Modified Python scripts and excel sheets that resulted in analysis and data collection to be faster, scalable and have better exception handling.",
-        "Qualified a new harness for laparoscopic devices through collaboration with multiple teams that reduced costs by 90% and improved RF energy efficiency.",
-        "Reduced costs, enhanced overall efficiency and customer ease of use by becoming the main point of contact between R&D teams, harness vendors and clinical team."
+        "Revamped Python scripts and excel sheets that resulted in analysis and data collection to be faster, scalable and have better exception handling.",
+        "Took on a pivotal role in a cross-functional collaboration effort involving R&D, Regulatory Analysis, Quality, and more, to successfully qualify a new harness for laparoscopic devices.",
+        "Achieved a remarkable 90% reduction in costs while simultaneously enhancing RF energy efficiency, contributing significantly to the company's financial and operational success."
       ]
     );
     let masters = new Job(
@@ -86,9 +87,12 @@ export class ExperienceComponent implements OnInit{
       "January 2023 - Present",
       "Software Engineer",
       [
-        "Built a workforce management solution for a time keeping company.",
-        "Revamped the frontend UI to use the latest frontend UX/UI designs using Angular and Ionic.",
-        "Configured MQTT broker/server with Mosquitto to send commands and receive events/messages from devices and automated data pipelining with Python scripting."
+        "Developed a workforce management solution for a time and attendance keeping company.",
+        "Designed and implemented a robust minimal API using ASP.Net Core Web API, SQL Server, and Swagger for seamless data interaction.",
+        "Revamped the frontend UI to align with latest UX/UI designs, utilizing Angular and Ionic frameworks.",
+        "Configured MQTT broker/server with Mosquitto to facilitate communication with devices, enabling command transmission and event handling.",
+        "Automated data pipelining through Python scripting, optimizing data flow efficiency.",
+
       ]
     );
     this.jobs.push(job5, masters, job4, job3, job2, job1, bachelors);
